44af2583a2fd1ad47a2e9588ac69c3594140e29c
wanfengaodaliya <2019193102@qq.com>
2026-08-13 12:29:10 +0800

    fix: preserve configured edge scheduler URL

--- cloud_edge_project/edge_service/app.py
+++ cloud_edge_project/edge_service/app.py
@@ -16,7 +16,7 @@ import numpy as np
 from fastapi import Body, FastAPI
 from fastapi.responses import JSONResponse
 
-from common.config import load_config
+from common.config import load_config, service_url
 from common.schemas import ContractError, error_response, is_v01_task_request
 from edge_service.model import EDGE_NODE_ID, infer_edge, infer_edge_v01
 
@@ -175,7 +175,7 @@ def _build_runtime():
     )
     scheduler_base = os.getenv(
         "SCHEDULER_SERVICE_BASE_URL",
-        "http://127.0.0.1:18011",
+        service_url("scheduler", config),
     )
     cloud_base = os.getenv("CLOUD_SERVICE_BASE_URL", "http://127.0.0.1:18021")
     runtime_config = EdgeRuntimeConfig(
