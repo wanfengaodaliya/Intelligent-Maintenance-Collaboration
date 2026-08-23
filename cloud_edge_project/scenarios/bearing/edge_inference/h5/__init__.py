c3f64f284877ca5dbe1aac0bf596af57de39aa53
XiangShiz <1402652948@qq.com>
2026-08-23 23:38:27 +0800

    refactor: move bearing h5 network implementation

--- cloud_edge_project/scenarios/bearing/edge_inference/h5/__init__.py
+++ cloud_edge_project/scenarios/bearing/edge_inference/h5/__init__.py
@@ -4,5 +4,6 @@ from scenarios.bearing.edge_inference.h5.features import (
     _compute_single,
     normalize_features,
 )
+from scenarios.bearing.edge_inference.h5.network import PhysicalFusionModel
 
-__all__ = ["_compute_single", "normalize_features"]
+__all__ = ["_compute_single", "normalize_features", "PhysicalFusionModel"]
