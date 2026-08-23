ee39a4d819b4588ce7b8475226791d5b03443b1e
XiangShiz <1402652948@qq.com>
2026-08-24 01:03:41 +0800

    refactor: move bearing cloud moment runtime

--- cloud_edge_project/compatibility/bearing_v12/cloud_moment_exports.py
+++ cloud_edge_project/compatibility/bearing_v12/cloud_moment_exports.py
@@ -3,6 +3,24 @@
 from scenarios.bearing.cloud_diagnosis.moment_backbone import (
     load_moment_backbone,
 )
+from scenarios.bearing.cloud_diagnosis.moment_light_adapt import (
+    LABEL_NAMES,
+    MODEL_VERSION,
+    MomentLightAdaptRunner,
+    MomentPrediction,
+    MomentReviewPolicy,
+    build_condition_vector,
+    deployment_workspace_root,
+)
 
 
-__all__ = ["load_moment_backbone"]
+__all__ = [
+    "load_moment_backbone",
+    "LABEL_NAMES",
+    "MODEL_VERSION",
+    "MomentPrediction",
+    "MomentReviewPolicy",
+    "build_condition_vector",
+    "deployment_workspace_root",
+    "MomentLightAdaptRunner",
+]
